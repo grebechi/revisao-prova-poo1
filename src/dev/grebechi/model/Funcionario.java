9cb5f260b0220bcca4c99e4cda5286525b1f0e20
Gabriel Rebechi <luiz.rebechi@doc360.com.br>
2025-05-01 15:37:27 -0300

    Create class Funcionario

---
+++ src/dev/grebechi/model/Funcionario.java
@@ -0,0 +1,38 @@
+package dev.grebechi.model;
+
+
+public class Funcionario extends Pessoa{
+    
+    private int matricula;
+    private double salario;
+
+    public Funcionario(String nome, String sobrenome, int matricula, double salario) {
+        super(nome, sobrenome);
+        this.setMatricula(matricula);
+        this.setSalario(salario);
+    }
+
+    public Funcionario(String nome, String sobrenome, Endereco endereco, int matricula, double salario) {
+        this(nome, sobrenome, matricula, salario);
+        this.setEndereco(endereco);
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    private void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+    
+
+    public double getSalario() {
+        return salario;
+    }
+
+    private void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+}
