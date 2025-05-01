5b769dd8ec4b66775b37baaf6613a8bd97eb6439
Gabriel Rebechi <luiz.rebechi@doc360.com.br>
2025-05-01 12:55:00 -0300

    feat: Create class StringUtils

---
+++ src/StringUtils.java
@@ -0,0 +1,8 @@
+
+public class StringUtils {
+    
+    public static String capitalizarNome(String str) {
+        if (str == null || str.isEmpty()) return str;
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+}
